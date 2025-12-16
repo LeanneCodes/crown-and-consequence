322cd074d4f9a7bc47fb281f50a2cc90cf3158c6
Olivia-it <olivia.gie@outlook.com>
2025-12-16 16:18:14 +0000

    naming changes

--- server/db/stories.sql
+++ server/db/stories.sql
@@ -11,7 +11,7 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL
 );
 
 
--- server/models/User.js
+++ server/models/User.js
@@ -1,34 +1,33 @@
 const db = require("../db/connect");
 
 class User {
-  constructor({ user_id, username, email, password, is_admin }) {
-    this.user_id = user_id;
+  constructor({ id, username, email, password}) {
+    this.id = id;
     this.username = username;
     this.email = email;
     this.password = password;
-    this.is_admin = is_admin
   }
 
   static async create(data) {  
        // Create a new user
-       const {username, email, password, isAdmin} = data;
-    let response = await db.query('INSERT INTO "user" (username, email, password) VALUES ($1, $2, $3) RETURNING user_id;',
+       const {username, email, password} = data;
+    let response = await db.query('INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id;',
       [username, email, password]);
- const newId = response.rows[0].user_id;
+ const newId = response.rows[0].id;
         const newUser = await User.findById(newId);
         return newUser;
   }
 
   static async findByUsername(username) {     // Find a user by email (used for login)
-    const response = await db.query('SELECT * FROM "user" WHERE username = $1;',
+    const response = await db.query('SELECT * FROM users WHERE username = $1;',
       [username]
     );
 
     return response.rows.length ? new User(response.rows[0]) : null;
   }
 
-  static async findById(user_id) {   // Find a user by ID (used for auth, progress, ownership checks)
-    const response = await db.query('SELECT * FROM "user" WHERE user_id = $1', [user_id]);
+  static async findById(id) {   // Find a user by ID (used for auth, progress, ownership checks)
+    const response = await db.query('SELECT * FROM users WHERE id = $1', [id]);
     if (response.rows.length === 0) {
             throw new Error("Unable to locate user.");
     }
@@ -37,7 +36,7 @@ class User {
 
   static async updatePassword(username, password) {   // Update a user's password (hashing done in controller)
     const response = await db.query(
-      `UPDATE "user"
+      `UPDATE users
       SET password = $1
       WHERE username = $2
       RETURNING *;
@@ -55,7 +54,7 @@ class User {
   static async deleteByUsername(username) {     // Delete a user account
     const response = await db.query(
       `
-      DELETE FROM "user"
+      DELETE FROM users
       WHERE username = $1
       RETURNING *;
       `,
