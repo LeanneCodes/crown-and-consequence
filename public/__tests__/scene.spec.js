32d5ecd0de3fc3785ecabfdf3a034654bf727560
MVCRUST <max.crust7@gmail.com>
2025-12-16 15:57:26 +0000

    Fixed the end and scene to match the html, therefore all tests are connected to the right files, we just need to add more tersts for them now

--- public/__tests__/scene.spec.js
+++ public/__tests__/scene.spec.js
@@ -10,10 +10,10 @@ describe('scene.html', () => {
     document = await dom.window.document;
   })
   
-  it('has a login button', () => {
-    const btn = document.querySelector('button')
-    expect(btn).toBeTruthy
-    expect(btn.innerHTML).toBe("Log In")
+  it('Heading matches', () => {
+    const h1 = document.querySelector('h1')
+    expect(h1).toBeTruthy
+    expect(h1.textContent).toBe("Explore the World")
   })
 
 //   it('h1 is empty when website loads', () => {
